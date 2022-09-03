bf5e3dfe2bebe07a250d2a20d1067dd685f35189
akm <akm2000@gmail.com>
2022-09-03 10:07:59 +0900

    :pencil2: テストを失敗を試すコメントを追加

--- ch01_first_test/main_test.go
+++ ch01_first_test/main_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-// Q1. 足し算を行う関数Addを、同じディレクトリのファイル add.go に作成してください
 func TestGolang(t *testing.T) {
+	// Suggest テストに失敗するようにパラメータや期待する結果を変更して、テストを実行してみてください。
 	s := Add(1, 2)
 	if s != 3 {
 		t.FailNow()
