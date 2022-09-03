28e115b953aea3cc076cb4cf8cf4fd3b04ed37fd
akm <akm2000@gmail.com>
2022-09-03 18:50:55 +0900

    :green_heart: Substractのテストを追加
    $ go test .
    --- FAIL: TestSubstract (0.00s)
    FAIL
    FAIL    github.com/akm/second_tour_of_go/ch01_first_test        0.408s
    FAIL

--- ch01_first_test/main_test.go
+++ ch01_first_test/main_test.go
@@ -10,3 +10,12 @@ func TestAdd(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestSubstract(t *testing.T) {
+	if s := Substract(1, 2); s != -1 {
+		t.FailNow()
+	}
+	if s := Substract(0, 3); s != -3 {
+		t.FailNow()
+	}
+}
