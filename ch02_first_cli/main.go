02cca3b4c4d0c2bc6babd5e679116dd032dbceb0
akm <akm2000@gmail.com>
2022-09-03 21:11:56 +0900

    :sparkles: add と substract を実装

--- ch02_first_cli/main.go
+++ ch02_first_cli/main.go
@@ -7,22 +7,45 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("USAGE:\n")
-		fmt.Printf("%s X Y\n", os.Args[0])
-		fmt.Printf("   Shows sum of X and Y\n")
-		fmt.Printf("   X and Y must be number\n")
+	if len(os.Args) < 4 {
+		showHelp()
 		os.Exit(1)
 	}
-	x, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("ERROR: %q is not a number\n", os.Args[1])
+	var kind string
+	switch os.Args[1] {
+	case "add":
+		kind = "addition"
+	case "substract":
+		kind = "substraction"
+	default:
+		showHelp()
 		os.Exit(1)
 	}
-	y, err := strconv.Atoi(os.Args[2])
+
+	x, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		fmt.Printf("ERROR: %q is not a number\n", os.Args[2])
 		os.Exit(1)
 	}
-	fmt.Printf("Result: %d\n", x+y)
+	y, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("ERROR: %q is not a number\n", os.Args[3])
+		os.Exit(1)
+	}
+
+	var res int
+	switch kind {
+	case "addition":
+		res = x + y
+	case "substraction":
+		res = x - y
+	}
+	fmt.Printf("%s: %d\n", kind, res)
+}
+
+func showHelp() {
+	fmt.Printf("USAGE:\n")
+	fmt.Printf("%s (add|substract) X Y\n", os.Args[0])
+	fmt.Printf("   Shows addion or substraction with X and Y\n")
+	fmt.Printf("   X and Y must be number\n")
 }
