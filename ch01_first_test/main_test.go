c1ab68bd55ed6c0c7dcbb5663ca19106e1725d1c
akm <akm2000@gmail.com>
2022-09-03 18:13:03 +0900

    :green_heart: MultiplyとDivideのテストを追加

--- ch01_first_test/main_test.go
+++ ch01_first_test/main_test.go
@@ -18,5 +18,26 @@ func TestSubstract(t *testing.T) {
 	}
 }
 
-// Q3. 掛け算 Multiply と 割り算 Divide についてもQ2 を同じ順番で実装してみましょう
+// Q3. 掛け算 Multiply と 割り算の商と剰余を求める関数 Divide についてもQ2 を同じ順番で実装してみましょう。
+//     (商は英語で quotient, 剰余は remainder と呼ばれ、qとrと略すことが多いです)
 //     一つのテストの中に複数回の関数の呼び出しと結果の確認のコードを書いてください。
+func TestMultiply(t *testing.T) {
+	if m := Multiply(6, 2); m != 12 {
+		t.FailNow()
+	}
+	if m := Multiply(0, 2); m != 0 {
+		t.FailNow()
+	}
+	if m := Multiply(100, 0); m != 0 {
+		t.FailNow()
+	}
+}
+
+func TestDivide(t *testing.T) {
+	if q, r := Divide(6, 2); q != 3 || r != 0 {
+		t.FailNow()
+	}
+	if q, r := Divide(7, 3); q != 2 || r != 1 {
+		t.FailNow()
+	}
+}
