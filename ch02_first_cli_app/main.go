b750171a0390ab59e0515c9839493d6f479ec4bd
akm <akm2000@gmail.com>
2022-09-04 20:22:36 +0900

    :sparkles: 足し算を実装

--- ch02_first_cli_app/main.go
+++ ch02_first_cli_app/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var name string
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	} else {
-		name = "someone"
+	if len(os.Args) < 3 {
+		fmt.Printf("USAGE:\n")
+		fmt.Printf("%s X Y\n", os.Args[0])
+		fmt.Printf("   Shows sum of X and Y\n")
+		fmt.Printf("   X and Y must be number\n")
+		os.Exit(1)
 	}
-	fmt.Printf("Hello, %s!", name)
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("ERROR: %q is not a number\n", os.Args[1])
+		os.Exit(1)
+	}
+	y, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("ERROR: %q is not a number\n", os.Args[2])
+		os.Exit(1)
+	}
+	fmt.Printf("Result: %d\n", x+y)
 }
