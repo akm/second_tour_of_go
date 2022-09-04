ca324c214e654547f5b3f0b4fa5fcec9b4e9eed6
akm <akm2000@gmail.com>
2022-09-04 20:25:03 +0900

    :recycle: parseInt関数を抽出

--- ch02_first_cli_app/main.go
+++ ch02_first_cli_app/main.go
@@ -14,15 +14,23 @@ func main() {
 		fmt.Printf("   X and Y must be number\n")
 		os.Exit(1)
 	}
-	x, err := strconv.Atoi(os.Args[1])
+	x, err := parseInt(os.Args[1])
 	if err != nil {
-		fmt.Printf("ERROR: %q is not a number\n", os.Args[1])
+		fmt.Printf("%s\n", err.Error())
 		os.Exit(1)
 	}
 	y, err := strconv.Atoi(os.Args[2])
 	if err != nil {
-		fmt.Printf("ERROR: %q is not a number\n", os.Args[2])
+		fmt.Printf("%s\n", err.Error())
 		os.Exit(1)
 	}
 	fmt.Printf("Result: %d\n", x+y)
+}
+
+func parseInt(s string) (int, error) {
+	r, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, fmt.Errorf("ERROR: %q is not a number", s)
+	}
+	return r, nil
 }
