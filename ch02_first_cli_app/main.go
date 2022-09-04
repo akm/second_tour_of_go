1e907f265b3793b4cbf17c86008fdecc56158ee4
akm <akm2000@gmail.com>
2022-09-04 20:48:51 +0900

    :sparkles: サブコマンド add と subtract を実装

--- ch02_first_cli_app/main.go
+++ ch02_first_cli_app/main.go
@@ -7,27 +7,47 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		showHelp()
 		os.Exit(1)
 	}
-	x, err := parseInt(os.Args[1])
+
+	var kind string
+	switch os.Args[1] {
+	case "add":
+		kind = "addition"
+	case "subtract":
+		kind = "subtraction"
+	default:
+		showHelp()
+		os.Exit(1)
+	}
+
+	x, err := parseInt(os.Args[2])
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
 		os.Exit(1)
 	}
-	y, err := strconv.Atoi(os.Args[2])
+	y, err := strconv.Atoi(os.Args[3])
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
 		os.Exit(1)
 	}
-	fmt.Printf("Result: %d\n", x+y)
+
+	var res int
+	switch kind {
+	case "addition":
+		res = x + y
+	case "subtraction":
+		res = x - y
+	}
+	fmt.Printf("%s: %d\n", kind, res)
 }
 
 func showHelp() {
 	fmt.Printf("USAGE:\n")
-	fmt.Printf("%s X Y\n", os.Args[0])
-	fmt.Printf("   Shows sum of X and Y\n")
+	fmt.Printf("%s (add|subtract) X Y\n", os.Args[0])
+	fmt.Printf("   Shows addition or subtraction with X and Y\n")
 	fmt.Printf("   X and Y must be number\n")
 }
 
