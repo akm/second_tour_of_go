3088c6df8f0f61c1f78ee8697d48cbe3ecd88efc
akm <akm2000@gmail.com>
2022-09-07 11:09:21 +0900

    :sparkles: Divisionの仮実装を追加

--- ch02_first_cli_app/calc.go
+++ ch02_first_cli_app/calc.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Result interface {
 	String() string
@@ -9,6 +12,7 @@ type Result interface {
 type Int int
 
 var _ Result = Int(0)
+
 func (i Int) String() string {
 	return strconv.Itoa(int(i))
 }
@@ -52,4 +56,35 @@ func (*Multiplication) String() string {
 
 func (*Multiplication) Do(x, y int) Result {
 	return Int(x * y)
+}
+
+type DivisionResult struct {
+	Quotient  int
+	Remainder int
+}
+
+var _ Result = (*DivisionResult)(nil)
+
+func (d *DivisionResult) String() string {
+	return fmt.Sprintf("quotient: %d, remainder: %d", d.Quotient, d.Remainder)
+}
+
+type InvalidDenominator struct{}
+
+var _ Result = (*InvalidDenominator)(nil)
+
+func (i *InvalidDenominator) String() string {
+	return "Invalid denominator. It must be not zero"
+}
+
+type Division struct{}
+
+var _ Calculation = (*Division)(nil)
+
+func (*Division) String() string {
+	return "division"
+}
+
+func (*Division) Do(x, y int) Result {
+	return Int(0)
 }
