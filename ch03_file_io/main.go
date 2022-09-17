acd5388ae7d7e3189f004a01b0cd00a557530d76
akm <akm2000@gmail.com>
2022-09-17 12:32:02 +0900

    :sparkles: writeサブコマンドを実装

--- ch03_file_io/main.go
+++ ch03_file_io/main.go
@@ -29,6 +29,21 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println(string(b))
+	case "write":
+		if len(os.Args) < 4 {
+			showHelp()
+			os.Exit(1)
+		}
+		f, err := os.Create(os.Args[2])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		if _, err := fmt.Fprintln(f, os.Args[3]); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	default:
 		showHelp()
 		os.Exit(1)
@@ -39,4 +54,6 @@ func showHelp() {
 	fmt.Printf("USAGE:\n")
 	fmt.Printf("%s cat FILE\n", os.Args[0])
 	fmt.Printf("   Shows the contents of FILE\n")
+	fmt.Printf("%s write FILE CONTENT\n", os.Args[0])
+	fmt.Printf("   Writes CONTENT to FILE\n")
 }
