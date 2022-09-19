0a7bc76a15cc95b7cc54217926676e4850c453c1
akm <akm2000@gmail.com>
2022-09-19 14:01:22 +0900

    :recycle: それぞれの型にコンストラクタが必要かどうかを判断し、必要なら追加
    ResponseItem.Calculateで行おうとしていた計算はそのコンストラクタでまとめられるので廃止

--- ch04_json/estimate.go
+++ ch04_json/estimate.go
@@ -2,11 +2,15 @@ package main
 
 import "time"
 
+// 商品の属性を表す型
+// 商品JSONファイルをUnmarshalして生成されるので、されるので、コンストラクタは不要
 type ProductAttrs struct {
 	UnitPrice   int
 	ReducedRate bool
 }
 
+// 商品名とProductAttrsを関連付けるmapを拡張した型
+// 商品JSONファイルをUnmarshalして生成されるので、されるので、コンストラクタは不要
 type ProductMap map[string]*ProductAttrs
 
 func (m ProductMap) Get(product string) *ProductAttrs {
@@ -17,16 +21,21 @@ func (m ProductMap) Calculate(req Request) *Response {
 	return nil
 }
 
+// 見積もりRequestを表す型
+// 見積もりRequestJSONファイルをUnmarshalして生成されるので、されるので、コンストラクタは不要
 type Request struct {
 	ClientName string
 	Items      []*RequestItem
 }
 
+// 見積もりの明細を表す型
+// 見積もりRequestJSONファイルをUnmarshalして生成されるので、されるので、コンストラクタは不要
 type RequestItem struct {
 	ProductName string
 	Quantity    int
 }
 
+// 見積もり結果を表す型
 type Response struct {
 	ClientName  string
 	EstimatedAt time.Time
@@ -36,9 +45,14 @@ type Response struct {
 	Items       []*ResponseItem
 }
 
+func NewResponse(clientName string) *Response {
+	return nil
+}
+
 func (m *Response) Calculate() {
 }
 
+// 見積もり結果の明細を表す型
 type ResponseItem struct {
 	ProductName string
 	Quantity    int
@@ -47,5 +61,6 @@ type ResponseItem struct {
 	Tax         int
 }
 
-func (m *ResponseItem) Calculate(attrs *ProductAttrs, quantity int) {
+func NewResponseItem(productName string, attrs *ProductAttrs, quantity int) *ResponseItem {
+	return nil
 }
