ba2c988f2a209ec72e92371ca9866c416432b89d
akm <akm2000@gmail.com>
2022-09-19 14:48:34 +0900

    :green_heart: 他のテストのパターンを追加する前に 既存のパターンを t.Run でグルーピング

--- ch04_json/estimate_test.go
+++ ch04_json/estimate_test.go
@@ -28,43 +28,44 @@ func TestProductMapGet(t *testing.T) {
 }
 
 func TestProductMapCalculate(t *testing.T) {
-	m := newTestProductMap()
-	res, err := m.Calculate(Request{
-		ClientName: "John Smith",
-		Items: []*RequestItem{
-			{ProductName: "Apple", Quantity: 2},
-			{ProductName: "Orange", Quantity: 3},
-			{ProductName: "Banana", Quantity: 4},
-		},
+	t.Run("basic pattern", func(t *testing.T) {
+		m := newTestProductMap()
+		res, err := m.Calculate(Request{
+			ClientName: "John Smith",
+			Items: []*RequestItem{
+				{ProductName: "Apple", Quantity: 2},
+				{ProductName: "Orange", Quantity: 3},
+				{ProductName: "Banana", Quantity: 4},
+			},
+		})
+		if err != nil {
+			t.Errorf("m.Calculate() = %v, want nil", err)
+		}
+		if res == nil {
+			t.Fatalf("m.Calculate() = nil, want not nil")
+		}
+		if res.ClientName != "John Smith" {
+			t.Errorf("res.ClientName = %v, want \"John Smith\"", res.ClientName)
+		}
+		if res.EstimatedAt.IsZero() {
+			t.Errorf("res.EstimatedAt = %v, want non-zero", res.EstimatedAt)
+		}
+		if res.SubTotal != 2*200+3*120+4*250 {
+			t.Errorf("res.SubTotal = %v, want %v", res.SubTotal, 2*200+3*120+4*250)
+		}
+		if res.Tax != 2*200*10/100+3*120*8/100+4*250*8/100 {
+			t.Errorf("res.Tax = %v, want %v", res.Tax, 2*200*10/100+3*120*8/100+4*250*8/100)
+		}
+		if res.Total != 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100 {
+			t.Errorf("res.Total = %v, want %v", res.Total, 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100)
+		}
+		if len(res.Items) != 3 {
+			t.Fatalf("len(res.Items) = %v, want 3", len(res.Items))
+		}
+		assertResponseItem(t, res.Items[0], "Apple", 2, 2*200, 10, 2*200*10/100)
+		assertResponseItem(t, res.Items[1], "Orange", 3, 3*120, 8, 3*120*8/100)
+		assertResponseItem(t, res.Items[2], "Banana", 4, 4*250, 8, 4*250*8/100)
 	})
-	if err != nil {
-		t.Errorf("m.Calculate() = %v, want nil", err)
-	}
-	if res == nil {
-		t.Fatalf("m.Calculate() = nil, want not nil")
-	}
-	if res.ClientName != "John Smith" {
-		t.Errorf("res.ClientName = %v, want \"John Smith\"", res.ClientName)
-	}
-	if res.EstimatedAt.IsZero() {
-		t.Errorf("res.EstimatedAt = %v, want non-zero", res.EstimatedAt)
-	}
-	if res.SubTotal != 2*200+3*120+4*250 {
-		t.Errorf("res.SubTotal = %v, want %v", res.SubTotal, 2*200+3*120+4*250)
-	}
-	if res.Tax != 2*200*10/100+3*120*8/100+4*250*8/100 {
-		t.Errorf("res.Tax = %v, want %v", res.Tax, 2*200*10/100+3*120*8/100+4*250*8/100)
-	}
-	if res.Total != 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100 {
-		t.Errorf("res.Total = %v, want %v", res.Total, 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100)
-	}
-	if len(res.Items) != 3 {
-		t.Fatalf("len(res.Items) = %v, want 3", len(res.Items))
-	}
-
-	assertResponseItem(t, res.Items[0], "Apple", 2, 2*200, 10, 2*200*10/100)
-	assertResponseItem(t, res.Items[1], "Orange", 3, 3*120, 8, 3*120*8/100)
-	assertResponseItem(t, res.Items[2], "Banana", 4, 4*250, 8, 4*250*8/100)
 }
 
 func assertResponseItem(t *testing.T, actual *ResponseItem, productName string, qty, subTotal, taxRate, tax int) {
