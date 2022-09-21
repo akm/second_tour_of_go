6b4606668a513bfd985d3037dc978cd2ad8339c6
akm <akm2000@gmail.com>
2022-09-21 09:46:04 +0900

    :green_heart: :recycle: testifyのassertとrequire を使うようにテストを変更

--- ch05_testify/estimate_test.go
+++ ch05_testify/estimate_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func newTestProductMap() ProductMap {
@@ -16,15 +19,15 @@ func newTestProductMap() ProductMap {
 
 func TestProductMapGet(t *testing.T) {
 	m := newTestProductMap()
-	if r := m.Get("Apple"); r == nil || r.UnitPrice != 200 || r.ReducedRate != false {
-		t.Errorf("m.Get(\"Apple\") = %v, want {UnitPrice: 200, ReducedRate: false}", r)
+	if r := m.Get("Apple"); assert.NotNil(t, r) {
+		assert.Equal(t, 200, r.UnitPrice)
+		assert.False(t, r.ReducedRate)
 	}
-	if r := m.Get("Banana"); r == nil || r.UnitPrice != 250 || r.ReducedRate != true {
-		t.Errorf("m.Get(\"Banana\") = %v, want {UnitPrice: 250, ReducedRate: true}", r)
+	if r := m.Get("Banana"); assert.NotNil(t, r) {
+		assert.Equal(t, 250, r.UnitPrice)
+		assert.True(t, r.ReducedRate)
 	}
-	if r := m.Get("Grape"); r != nil {
-		t.Errorf("m.Get(\"Grape\") = %v, want nil", r)
-	}
+	require.Nil(t, m.Get("Grape"))
 }
 
 func TestProductMapCalculate(t *testing.T) {
@@ -38,33 +41,18 @@ func TestProductMapCalculate(t *testing.T) {
 				{ProductName: "Banana", Quantity: 4},
 			},
 		})
-		if err != nil {
-			t.Errorf("m.Calculate() = %v, want nil", err)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		assert.Equal(t, "John Smith", res.ClientName)
+		assert.False(t, res.EstimatedAt.IsZero())
+		assert.Equal(t, 2*200+3*120+4*250, res.SubTotal)
+		assert.Equal(t, 2*200*10/100+3*120*8/100+4*250*8/100, res.Tax)
+		assert.Equal(t, 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100, res.Total)
+		if assert.Len(t, res.Items, 3) {
+			assertResponseItem(t, res.Items[0], "Apple", 2, 2*200, 10, 2*200*10/100)
+			assertResponseItem(t, res.Items[1], "Orange", 3, 3*120, 8, 3*120*8/100)
+			assertResponseItem(t, res.Items[2], "Banana", 4, 4*250, 8, 4*250*8/100)
 		}
-		if res == nil {
-			t.Fatalf("m.Calculate() = nil, want not nil")
-		}
-		if res.ClientName != "John Smith" {
-			t.Errorf("res.ClientName = %v, want \"John Smith\"", res.ClientName)
-		}
-		if res.EstimatedAt.IsZero() {
-			t.Errorf("res.EstimatedAt = %v, want non-zero", res.EstimatedAt)
-		}
-		if res.SubTotal != 2*200+3*120+4*250 {
-			t.Errorf("res.SubTotal = %v, want %v", res.SubTotal, 2*200+3*120+4*250)
-		}
-		if res.Tax != 2*200*10/100+3*120*8/100+4*250*8/100 {
-			t.Errorf("res.Tax = %v, want %v", res.Tax, 2*200*10/100+3*120*8/100+4*250*8/100)
-		}
-		if res.Total != 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100 {
-			t.Errorf("res.Total = %v, want %v", res.Total, 2*200+3*120+4*250+2*200*10/100+3*120*8/100+4*250*8/100)
-		}
-		if len(res.Items) != 3 {
-			t.Fatalf("len(res.Items) = %v, want 3", len(res.Items))
-		}
-		assertResponseItem(t, res.Items[0], "Apple", 2, 2*200, 10, 2*200*10/100)
-		assertResponseItem(t, res.Items[1], "Orange", 3, 3*120, 8, 3*120*8/100)
-		assertResponseItem(t, res.Items[2], "Banana", 4, 4*250, 8, 4*250*8/100)
 	})
 
 	t.Run("including unknown product", func(t *testing.T) {
@@ -77,31 +65,17 @@ func TestProductMapCalculate(t *testing.T) {
 				{ProductName: "Banana", Quantity: 4},
 			},
 		})
-		if err == nil {
-			t.Errorf("m.Calculate() should be return an error")
-		}
+		require.Error(t, err)
 	})
 }
 
 func assertResponseItem(t *testing.T, actual *ResponseItem, productName string, qty, subTotal, taxRate, tax int) {
-	if actual == nil {
-		t.Fatalf("actual = nil, want not nil")
-	}
-	if actual.ProductName != productName {
-		t.Errorf("actual.ProductName = %v, want %v", actual.ProductName, productName)
-	}
-	if actual.Quantity != qty {
-		t.Errorf("actual.Quantity = %v, want %v", actual.Quantity, qty)
-	}
-	if actual.SubTotal != subTotal {
-		t.Errorf("actual.SubTotal = %v, want %v", actual.SubTotal, subTotal)
-	}
-	if actual.TaxRate != taxRate {
-		t.Errorf("actual.TaxRate = %v, want %v", actual.TaxRate, taxRate)
-	}
-	if actual.Tax != tax {
-		t.Errorf("actual.Tax = %v, want %v", actual.Tax, tax)
-	}
+	require.NotNil(t, actual)
+	assert.Equal(t, productName, actual.ProductName)
+	assert.Equal(t, qty, actual.Quantity)
+	assert.Equal(t, subTotal, actual.SubTotal)
+	assert.Equal(t, taxRate, actual.TaxRate)
+	assert.Equal(t, tax, actual.Tax)
 }
 
 func TestNewResponseItem(t *testing.T) {
