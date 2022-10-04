e9b597a19778b6ae3790fcf7fbebf839e18e5d67
akm <akm2000@gmail.com>
2022-10-05 08:39:21 +0900

    :recycle: NewCircleを使うようにmain.goを変更

--- ch00_principle_review/main.go
+++ ch00_principle_review/main.go
@@ -6,7 +6,7 @@ func main() {
 	p1 := &Point{Px: 100, Py: 50}
 	fmt.Printf("%#v\n", p1)
 
-	c1 := &Circle{Point: p1, radius: 30}
+	c1 := NewCircle(p1, 30)
 	fmt.Printf("%#v\n", c1)
 
 	r1 := &Rect{Point: p1, width: 20, length: 10}
