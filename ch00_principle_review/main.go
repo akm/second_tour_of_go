c450ebd7f424d4fff0c538ce66e9a4eac9ac76bf
akm <akm2000@gmail.com>
2022-10-05 08:37:22 +0900

    :recycle: new ではなくて & を使うように変更

--- ch00_principle_review/main.go
+++ ch00_principle_review/main.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	p1 := new(Point)
-	p1.Px = 100
-	p1.Py = 50
+	p1 := &Point{Px: 100, Py: 50}
 	fmt.Printf("%#v\n", p1)
 
-	c1 := new(Circle)
-	c1.Point = p1
-	c1.radius = 30
+	c1 := &Circle{Point: p1, radius: 30}
 	fmt.Printf("%#v\n", c1)
 
-	r1 := new(Rect)
-	r1.Point = p1
-	r1.width = 20
-	r1.length = 10
+	r1 := &Rect{Point: p1, width: 20, length: 10}
 	fmt.Printf("%#v\n", r1)
 }
