aa14c01ad054153c009a1fd2ad5aed5803edbec5
akm <akm2000@gmail.com>
2022-10-10 14:43:03 +0900

    :green_heart: :recycle: 少ない行数で表現できるように関数を返すように変更

--- ch06_http_server/main_test.go
+++ ch06_http_server/main_test.go
@@ -11,47 +11,35 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-	assertTextResponse := func(t *testing.T, method, path string, result string) {
-		w := httptest.NewRecorder()
-		handler(w, httptest.NewRequest(method, path, nil))
-		resp := w.Result()
-		assert.Equal(t, http.StatusOK, resp.StatusCode)
-		body, err := ioutil.ReadAll(resp.Body)
-		assert.NoError(t, err)
-		assert.Equal(t, result, strings.TrimSpace(string(body)))
+	textResponse := func(method, path string, result string) func(t *testing.T) {
+		return func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler(w, httptest.NewRequest(method, path, nil))
+			resp := w.Result()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+			body, err := ioutil.ReadAll(resp.Body)
+			assert.NoError(t, err)
+			assert.Equal(t, result, strings.TrimSpace(string(body)))
+		}
 	}
 
-	assertError := func(t *testing.T, method, path string, status int) {
-		w := httptest.NewRecorder()
-		handler(w, httptest.NewRequest(method, path, nil))
-		resp := w.Result()
-		assert.Equal(t, status, resp.StatusCode)
+	errorResponse := func(method, path string, status int) func(t *testing.T) {
+		return func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler(w, httptest.NewRequest(method, path, nil))
+			resp := w.Result()
+			assert.Equal(t, status, resp.StatusCode)
+		}
 	}
 
 	t.Run("add", func(t *testing.T) {
-		t.Run("GET", func(t *testing.T) {
-			assertTextResponse(t, "GET", "/add?a=1&b=2", "3")
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertTextResponse(t, "GET", "/add?a=100&b=200", "300")
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertError(t, "GET", "/add", http.StatusBadRequest)
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertError(t, "GET", "/add?a=100", http.StatusBadRequest)
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertError(t, "GET", "/add?b=100", http.StatusBadRequest)
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertError(t, "GET", "/add?a=foo&bar=200", http.StatusBadRequest)
-		})
-		t.Run("GET", func(t *testing.T) {
-			assertError(t, "GET", "/add?a=100&bar=bar", http.StatusBadRequest)
-		})
-		t.Run("POST", func(t *testing.T) {
-			assertError(t, "POST", "/add?a=1&b=2", http.StatusMethodNotAllowed)
-		})
+		t.Run("GET", textResponse("GET", "/add?a=1&b=2", "3"))
+		t.Run("GET", textResponse("GET", "/add?a=100&b=200", "300"))
+		t.Run("GET", errorResponse("GET", "/add", http.StatusBadRequest))
+		t.Run("GET", errorResponse("GET", "/add?a=100", http.StatusBadRequest))
+		t.Run("GET", errorResponse("GET", "/add?b=100", http.StatusBadRequest))
+		t.Run("GET", errorResponse("GET", "/add?a=foo&bar=200", http.StatusBadRequest))
+		t.Run("GET", errorResponse("GET", "/add?a=100&bar=bar", http.StatusBadRequest))
+		t.Run("POST", errorResponse("POST", "/add?a=1&b=2", http.StatusMethodNotAllowed))
 	})
 }
